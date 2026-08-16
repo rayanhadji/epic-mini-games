136cca7296bc19be0c94f7a5b15254d76a092e88
rayanhadji <rayanhadji.fr@gmail.com>
2026-08-16 02:43:53 -0700

    resizing expirement

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ window = tk.Tk()
 window.title("Epic Mini Games")
 window.geometry("600x400")
 window.config(bg="grey")
+window.minsize(600, 400)
 
 #Creating frames
 menu_frame = tk.Frame(window, bg="blue")
@@ -15,14 +16,22 @@ rps_frame = tk.Frame(window, bg="purple")
 guess_number_frame = tk.Frame(window, bg="black")
 
 #Main menu
+menu_frame.grid_rowconfigure(0, weight=0)
+menu_frame.grid_rowconfigure(1, weight=1)
+menu_frame.grid_columnconfigure(0, weight=1)
+menu_frame.grid_columnconfigure(1, weight=3)
+
 title = tk.Label(menu_frame, text="Epic Mini Games", font=("Arial",24), bg="black", fg="white")
-title.pack(fill="x", pady=20)
+title.grid(row=0, column=0, columnspan=2, pady=20)
+
+menu_sidebar = tk.Frame(menu_frame, bg="blue")
+menu_sidebar.grid(row=1, column=0, sticky="nsew")
+
+menu_content = tk.Frame(menu_frame, bg="blue")
+menu_content.grid(row=1, column=1, sticky="nsew")
+
 
 menu_frame.pack(fill="both", expand=True)
-menu_sidebar = tk.Frame(menu_frame, bg="blue", width=150)
-menu_sidebar.pack(side="left", fill="y")
-menu_sidebar.pack_propagate(False)
-
 #Show frame function
 def show_frame(frame_to_show):
     menu_frame.pack_forget()
@@ -36,19 +45,19 @@ def show_frame(frame_to_show):
 
 #Menu Buttons + Exit Button
 play_button = tk.Button(menu_sidebar, text="Play", command=lambda: show_frame(play_frame), width=15, height=2)
-play_button.pack(pady=10)
+play_button.pack(pady=10, fill="x", padx=10)
 
 show_score_button = tk.Button(menu_sidebar, text="Show Score", command=lambda: show_frame(show_score_frame), width=15, height=2)
-show_score_button.pack(pady=10)
+show_score_button.pack(pady=10, fill="x", padx=10)
 
 reset_score_button = tk.Button(menu_sidebar, text="Reset Score", command=lambda: show_frame(reset_score_frame), width=15, height=2)
-reset_score_button.pack(pady=10)
+reset_score_button.pack(pady=10, fill="x", padx=10)
 
 def exit_button_press():
     window.destroy()
 
 exit_button = tk.Button(menu_sidebar, text="Exit", command=exit_button_press, width=15, height=2)
-exit_button.pack(pady=10)
+exit_button.pack(pady=10, fill="x", padx=10)
 
 
 #Play frame
