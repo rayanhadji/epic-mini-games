00a2004eed65cf6455a077a30da006e9e0e00796
rayanhadji <rayanhadji.fr@gmail.com>
2026-08-29 21:31:07 -0700

    added rock paper scissor game

--- main.py
+++ main.py
@@ -82,17 +82,92 @@ back_from_reset_score_button = tk.Button(reset_score_frame, text="Back", command
 back_from_reset_score_button.pack(pady=10)
 #============================================================================================================================================
 
+#rock paper scissor game=====================================================================================================================
+user_score = 0
+pc_score = 0
+
+#choices functions
+def choose_rock():
+    global user_score, pc_score
+    pc_choice = random.choice(["rock", "paper", "scissor"])
+    
+    if pc_choice == "rock":
+        rps_result_label.config(text="pc choosed rock, it's a tie")
+    elif pc_choice == "paper":
+        rps_result_label.config(text="pc choosed paper, you lost try again")
+        pc_score +=1
+    else:
+        rps_result_label.config(text="pc choosed scissor, congratulations you won")
+        user_score +=1
+    
+    rps_score_label.config(text=f"you: {user_score} - PC: {pc_score}")
+    
+def choose_paper():
+    global user_score, pc_score
+    pc_choice = random.choice(["rock", "paper", "scissor"])
+    
+    if pc_choice == "paper":
+        rps_result_label.config(text="pc choosed paper, it's a tie")
+    elif pc_choice == "scissor":
+        rps_result_label.config(text="pc choosed scissor, you lost try again")
+        pc_score +=1
+    else:
+        rps_result_label.config(text="pc choosed rock, congratulations you won")
+        user_score +=1
+    
+    rps_score_label.config(text=f"you: {user_score} - PC: {pc_score}")
+    
+def choose_scissor():
+    global user_score, pc_score
+    pc_choice = random.choice(["rock", "paper", "scissor"])
+    
+    if pc_choice == "scissor":
+        rps_result_label.config(text="pc choosed scissor, it's a tie")
+    elif pc_choice == "rock":
+        rps_result_label.config(text="pc choosed rock, you lost try again")
+        pc_score +=1
+    else:
+        rps_result_label.config(text="pc choosed paper, congratulations you won")
+        user_score +=1
+    
+    rps_score_label.config(text=f"you: {user_score} - PC: {pc_score}")
+
+#creating images
+rock_image = tk.PhotoImage(file="images/rock.png")
+paper_image = tk.PhotoImage(file="images/paper.png")
+scissor_image = tk.PhotoImage(file="images/scissor.png")
+
+#rock paper scissor game insides
+rps_score_label = tk.Label(rps_frame, text=f"you: {user_score} - PC: {pc_score}", font=("Arial", 14), bg="black", fg="white")
+rps_score_label.pack(pady=10)
+
+rps_result_label = tk.Label(rps_frame, text="", font=("Arial", 14), bg="black", fg="white")
+rps_result_label.pack(pady=10)
+
+rock_button = tk.Button(rps_frame, image=rock_image, command=choose_rock, width=100, height=100)
+rock_button.pack(pady=10)
+
+paper_button = tk.Button(rps_frame, image=paper_image, command=choose_paper, width=100, height=100)
+paper_button.pack(pady=10)
+
+scissor_button = tk.Button(rps_frame, image=scissor_image, command=choose_scissor, width=100, height=100)
+scissor_button.pack(pady=10)
+
+rps_game_back_button = tk.Button(rps_frame, text="Back", command=lambda: show_frame(play_frame), width=10, height=2)
+rps_game_back_button.pack(pady=10)
+#============================================================================================================================================
+
 #guess the number game=======================================================================================================================
-#start function
 attempts = 0
 target_number = 0
 
+#start function
 def start_guess_game():
     global attempts, target_number
     target_number = random.randint(1, 100)
     attempts = 0
     
-    result_label.config(text="Guess a number")
+    guess_result_label.config(text="Guess a number")
     guess_entry.delete(0, "end")
     guess_button.config(state="normal")
     
@@ -102,23 +177,23 @@ def check_guess():
     try:
         user_guess = int(guess_entry.get())
     except ValueError:
-        result_label.config(text="That's not a number!")
+        guess_result_label.config(text="That's not a number!")
         return
     
     attempts += 1
     guess_entry.delete(0, "end")
         
     if user_guess == target_number:
-        result_label.config(text=f"congratulations, you found it in {attempts} attempts")
+        guess_result_label.config(text=f"congratulations, you found it in {attempts} attempts")
         guess_button.config(state="disabled")
     elif user_guess > target_number:
-        result_label.config(text="Try a bit lower")
+        guess_result_label.config(text="Try a bit lower")
     else:
-        result_label.config(text="Try a bit higher")
+        guess_result_label.config(text="Try a bit higher")
     
 #guess the number game insides    
-result_label = tk.Label(guess_number_frame, text="Guess a number", font=("Arial", 14), bg="black", fg="white")
-result_label.pack(pady=10)
+guess_result_label = tk.Label(guess_number_frame, text="Guess a number", font=("Arial", 14), bg="black", fg="white")
+guess_result_label.pack(pady=10)
 
 guess_entry = tk.Entry(guess_number_frame, font=("Arial", 16), width=10)
 guess_entry.pack(pady=10)
@@ -129,7 +204,7 @@ guess_button.pack(pady=10)
 new_game_button = tk.Button(guess_number_frame, text="New Game", command=start_guess_game, width=10, height=2)
 new_game_button.pack(pady=10)
 
-back_button = tk.Button(guess_number_frame, text="Back", command=lambda: show_frame(play_frame), width=10, height=2)
-back_button.pack(pady=10)
+guess_game_back_button = tk.Button(guess_number_frame, text="Back", command=lambda: show_frame(play_frame), width=10, height=2)
+guess_game_back_button.pack(pady=10)
 #============================================================================================================================================
 window.mainloop()
